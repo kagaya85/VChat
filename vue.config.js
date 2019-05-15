fd9378aa683ae266f374d50088e4c182accdab3a
Kagaya <kagaya85@outlook.com>
2019-05-15 13:19:03 +0800

    add i18n

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
 module.exports = {
-    publicPath: './',  
-    devServer: {
-        port: 2333
-      }
+  publicPath: './',
+
+  devServer: {
+      port: 2333
+    },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true
     }
+  }
+}
